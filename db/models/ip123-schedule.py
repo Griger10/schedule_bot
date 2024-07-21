6ccfec7acef0840a141c4fb9f098d9dba396a8fe
Grisha Gertzik <griger1011@gmail.com>
2024-07-21 23:01:00 +0300

    3 model files created

--- db/models/ip123-schedule.py
+++ db/models/ip123-schedule.py
@@ -1,11 +1,40 @@
+import enum
+
 from db.base import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+class Days(enum.Enum):
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+
+
+class LessonTypes(enum.Enum):
+    NUMERATOR = 1
+    DENOMINATOR = 2
+
+
+class LessonNumbers(enum.Enum):
+    FIRST = 1
+    SECOND = 2
+    THIRD = 3
+    FOURTH = 4
+    FIFTH = 5
 
 
 class Schedule(Base):
     __tablename__ = 'ip123-schedule'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    day: Mapped[int]
-    number_of_lesson: Mapped[int]
+    day: Mapped[Days]
+    number_of_lesson: Mapped[LessonNumbers]
+    audience: Mapped[str]
+    lesson_id: Mapped[int] = mapped_column(ForeignKey("lesson.id"))
+    type: Mapped[LessonTypes | None] = mapped_column(nullable=True)
+
+
 
