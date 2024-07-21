120e06f6b19aac57a04ad16f3cbd9c58970ecd50
Grisha Gertzik <griger1011@gmail.com>
2024-07-21 12:58:23 +0300

    main.py and user_handlers update

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,6 +1,7 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, User
+from aiogram.types import Message, CallbackQuery
+from keyboards import build_command_keyboard
 from lexicon.lexicon import LEXICON
 
 router = Router()
@@ -18,10 +19,49 @@ async def help_handler(message: Message):
 
 @router.message(Command(commands=['monday']))
 async def monday_handler(message: Message):
-    await message.answer(text='Выберите тип недели', reply_markup=None)
+    await message.answer(text='Выберите тип недели',
+                         reply_markup=build_command_keyboard
+                         ('monday-numerator',
+                          'monday-denominator')
+                         )
 
 
+@router.message(Command(commands=['tuesday']))
+async def tuesday_handler(message: Message):
+    await message.answer(text='Выберите тип недели',
+                         reply_markup=build_command_keyboard
+                         ('tuesday-numerator',
+                          'tuesday-denominator')
+                         )
 
 
+@router.message(Command(commands=['wednesday']))
+async def tuesday_handler(message: Message):
+    await message.answer(text='Выберите тип недели',
+                         reply_markup=build_command_keyboard
+                         ('wednesday-numerator',
+                          'wednesday-denominator')
+                         )
 
 
+@router.message(Command(commands=['thursday']))
+async def tuesday_handler(message: Message):
+    await message.answer(text='Выберите тип недели',
+                         reply_markup=build_command_keyboard
+                         ('thursday-numerator',
+                          'thursday-denominator')
+                         )
+
+
+@router.message(Command(commands=['tuesday']))
+async def tuesday_handler(message: Message):
+    await message.answer(text='Выберите тип недели',
+                         reply_markup=build_command_keyboard
+                         ('tuesday-numerator',
+                          'tuesday-denominator')
+                         )
+
+
+@router.callback_query(F.data == 'monday-numerator')
+async def monday_schedule_handler(callback_query: CallbackQuery):
+    await callback_query.answer('Это расписание на понедельник')
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from main_menu import set_main_menu
 from config_data.config import load_config, Config
+from handlers import user_handlers, other_handlers
 
 
 async def main():
@@ -14,11 +15,12 @@ async def main():
 
     await set_main_menu(bot)
 
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
