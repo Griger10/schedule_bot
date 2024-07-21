c1561a87165f5e0ebe9f3a23ffccdf908a325058
Grisha Gertzik <griger1011@gmail.com>
2024-07-21 13:49:21 +0300

    gitignore, main.py and read_config updates

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from keyboards.main_menu import set_main_menu
-from config_data.config import load_config, Config
+from read_config import get_config, BotConfig
 from handlers import user_handlers, other_handlers
 
 
 async def main():
-    config: Config = load_config()
 
-    bot = Bot(token=config.tg_bot.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    bot_config = get_config(BotConfig, "bot")
+
+    bot = Bot(token=bot_config.token.get_secret_value(), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
 
     await set_main_menu(bot)
---
+++ read_config.py
@@ -0,0 +1,45 @@
+from functools import lru_cache
+from os import getenv
+from typing import TypeVar, Type
+
+from environs import Env
+from pydantic import BaseModel, SecretStr, PostgresDsn
+from yaml import load
+
+try:
+    from yaml import CSafeLoader as SafeLoader
+except ImportError:
+    from yaml import SafeLoader
+
+ConfigType = TypeVar("ConfigType", bound=BaseModel)
+
+
+class BotConfig(BaseModel):
+    token: SecretStr
+
+
+class DbConfig(BaseModel):
+    dsn: PostgresDsn
+    is_echo: bool
+
+
+@lru_cache
+def parse_config_file(path: str | None = None) -> dict:
+    env = Env()
+    env.read_env(path)
+    file_path = env("BOT_CONFIG")
+    if file_path is None:
+        error = "Could not find settings file"
+        raise ValueError(error)
+    with open(file_path, "rb") as file:
+        config_data = load(file, Loader=SafeLoader)
+    return config_data
+
+
+@lru_cache
+def get_config(model: Type[ConfigType], root_key: str) -> ConfigType:
+    config_dict = parse_config_file()
+    if root_key not in config_dict:
+        error = f"Key {root_key} not found"
+        raise ValueError(error)
+    return model.model_validate(config_dict[root_key])
