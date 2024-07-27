fc42f8f469f1b1fca2a7d7fda97006436007c2e7
Grisha Gertzik <griger1011@gmail.com>
2024-07-27 19:59:53 +0300

    all users middleware update

--- db/queries.py
+++ db/queries.py
@@ -1,2 +1,9 @@
+from db import User
+from sqlalchemy.dialects.postgresql import insert as upsert
+
+
 async def upsert_user(session, telegram_id, first_name, group):
-    pass
+    stmt = upsert(User).values(telegram_id=telegram_id,
+                               first_name=first_name,
+                               group=group)
+    stmt.
--- middlewares/track_all_users.py
+++ middlewares/track_all_users.py
@@ -1,6 +1,7 @@
 from typing import cast
 from aiogram import BaseMiddleware
 from aiogram.types import Message
+from cachetools import TTLCache
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.queries import upsert_user
@@ -9,18 +10,23 @@ from db.queries import upsert_user
 class TrackAllUsersMiddleware(BaseMiddleware):
     def __init__(self):
         super().__init__()
+        self.cache = TTLCache(
+            maxsize=1000,
+            ttl=60 * 60 * 24,  # 6 часов
+        )
 
     async def __call__(self, handler, event, data, ):
         event = cast(Message, event)
-        session: AsyncSession = data["session"]
-        try:
+        user_id = event.message.from_user.id
+
+        if user_id not in self.cache:
+            session: AsyncSession = data["session"]
             await upsert_user(
                 session=session,
                 telegram_id=event.from_user.id,
                 first_name=event.from_user.first_name,
                 group=None,
-                )
-        except Exception as e:
-            print(e)
+            )
+            self.cache[user_id] = None
 
         return await handler(event, data)
